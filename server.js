3c9d61c26bb6ee6e3a16b25e6cac412317aa7bed
gashirar <spin32632@gmail.com>
2019-01-27 04:39:44 -0800

    Add NO_AUTH option

--- server.js
+++ server.js
@@ -29,18 +29,22 @@ function start(port, host, argv) {
     return cliManager.initializeCli(argv).then(function() {
         const application = container.get(BackendApplication);
         application.use((req, res, next) => {
-            const auth = {
-                login: process.env.USER || 'user',
-                password: process.env.PASSWORD || 'P@ssw0rd'
-            }
-            const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
-            const [login, password] = new Buffer(b64auth, 'base64').toString().split(':')
-            if (!login || !password || login !== auth.login || password !== auth.password) {
-                res.set('WWW-Authenticate', 'Basic realm="401"')
-                res.status(401).send('Authentication required.')
-                return
-            }
-            next()
+            if (process.env.NO_AUTH === 'true') {
+		        next();
+			} else {
+                const auth = {
+                    login: process.env.USER || 'user',
+                    password: process.env.PASSWORD || 'P@ssw0rd'
+                }
+                const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+                const [login, password] = new Buffer(b64auth, 'base64').toString().split(':')
+                if (!login || !password || login !== auth.login || password !== auth.password) {
+                    res.set('WWW-Authenticate', 'Basic realm="401"')
+                    res.status(401).send('Authentication required.')
+                    return
+                }
+                next()
+			}
         });
         application.use(express.static(path.join(__dirname, '../../lib'), {
             index: 'index.html'
